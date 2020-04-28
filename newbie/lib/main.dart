475b4a357953805bcd5e3e6bab88912279288ba6
Saumitra Sapre <saumitrasapre69@gmail.com>
2020-04-28 21:44:25 +0530

    Hot Reload and Images within Flutter

--- newbie/lib/main.dart
+++ newbie/lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Grey Racoon'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child:Text('Racoons are fluffy!') ,
-    ),
-    floatingActionButton:FloatingActionButton(
-      child: Text('click'),
-    ),
-  ),
+  home: Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Grey Racoon'),
+        centerTitle: true,
+        backgroundColor: Colors.purple[600],
+      ),
+      body: Center(
+        /*child:Text(
+          'Raccoons are fluffy!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'Bangers',
+          ),
+        ) ,*/
+        child: Image(
+          //image: NetworkImage('https://curiodyssey.org/wp-content/uploads/bb-plugin/cache/xMammals-Raccoon-square.jpg.pagespeed.ic.W5-0FMTZBa.jpg'),
+          image: AssetImage('assets/raccoon2.jpg'),
+        ),
+      ),
+
+      floatingActionButton:FloatingActionButton(
+        child: Text('Click!'),
+        onPressed: () {},
+        backgroundColor: Colors.purple[600],
+      ),
+    );
+  }
+}
